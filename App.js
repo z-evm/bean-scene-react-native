77365e7b237234cf8fb319145e487bbe90d04e49
Alexander Maniago <alexandermaniago@gmail.com>
2024-10-19 14:45:28 +1100

    Created blank pages for the app and installed dependecies for the bottom navigation bar

--- App.js
+++ App.js
@@ -1,6 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import HomeScreen from './app/index';
+import MenuScreen from './app/menu';
+import OrderScreen from './app/order';
+import ReservationScreen from './app/reservation';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Menu Page" component={MenuScreen} />
+        <Tab.Screen name="Order Page" component={OrderScreen} />
+        <Tab.Screen name="Reservation Page" component={ReservationScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+/*
 export default function App() {
   return (
     <View style={styles.container}>
@@ -9,7 +32,7 @@ export default function App() {
     </View>
   );
 }
-
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
