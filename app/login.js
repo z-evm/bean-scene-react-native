4c2d3689c6a1528f4e6865b7e75790b8f81add6a
Alexander Maniago <alexandermaniago@gmail.com>
2024-11-21 14:04:09 +1100

    Login page

---
+++ app/login.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import {
+SafeAreaView,
+ScrollView,
+StatusBar,
+StyleSheet,
+Text,
+useColorScheme,
+Image,
+View,
+TextInput,
+TouchableOpacity,
+} from 'react-native';
+export function Login({ route, navigation }) {
+const [errors, setErrors] = useState({});
+const [state,setState] = useState({
+username: '',
+password: '',
+})
+const resetLoginData = () => { // clears order after every submit
+    setState({
+      username: '',
+        password: '',
+    });
+};
+const handleInputChange = (field, value) => {
+  setState({ ...state, [field]: value });
+
+  // Clear the error for the specific field being modified
+  setErrors((prevErrors) => {
+    const updatedErrors = { ...prevErrors };
+    delete updatedErrors[field]; // Remove the error for the current field
+    return updatedErrors;
+  });
+};
+// /auth/user/login
+const onPressLogin = async () => {
+// Do something about login operation
+  const newLogin  = {
+    email: state.username,
+    password: state.password,
+  };
+  const newErrors = {};
+try {
+  if (!state.username.trim()) {
+    newErrors.username = 'Email is required.';
+  }
+  if (!state.password.trim()) {
+    newErrors.password = 'Password is required.';
+  }
+  if (Object.keys(newErrors).length > 0) {
+    setErrors(newErrors); // Set errors in state
+    return; // Stop further execution
+  }
+    const response = await fetch(`http://localhost:3000/auth/user/login`, { 
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newLogin),
+    });
+    if (response.ok) {
+      console.log("Successful Login")
+      resetLoginData();
+      navigation.navigate('Floor');
+    } else {
+      console.error('Unsuccessful Login', response.statusText);
+    }
+  } catch (error) {
+    console.error('Unsuccessful Login:', error);
+  }
+};
+return (
+<View style={styles.container}>
+<Image style={styles.logo} source={require('../assets/bean-scene-logo.png')} />
+<View style={styles.inputView}>
+<TextInput
+style={styles.inputText}
+placeholder="Username"
+placeholderTextColor="#003f5c"
+onChangeText={(text) => handleInputChange('username', text)}/>
+{errors.username && <Text style={styles.errorText}>{errors.username}</Text>}
+</View>
+<View style={styles.inputView}>
+<TextInput
+style={styles.inputText}
+secureTextEntry
+placeholder="Password"
+placeholderTextColor="#003f5c"
+onChangeText={(text) => handleInputChange('password', text)}/>
+{errors.password && <Text style={styles.errorText}>{errors.password}</Text>}
+</View>
+<TouchableOpacity
+onPress = {onPressLogin}
+style={styles.loginBtn}>
+<Text style={styles.loginText}>LOGIN </Text>
+</TouchableOpacity>
+</View>
+);
+}
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+backgroundColor: '#F0EAD6',
+alignItems: 'center',
+justifyContent: 'center',
+},
+logo: {
+    width:293,
+    height:106,
+    marginBottom:40
+},
+title:{
+fontWeight: "bold",
+fontSize:50,
+color:"#fb5b5a",
+marginBottom: 40,
+},
+inputView:{
+width:"80%",
+backgroundColor:"#FFF",
+borderRadius:25,
+height:50,
+marginBottom:20,
+justifyContent:"center",
+padding:20
+},
+inputText:{
+height:50,
+color:"#000"
+},
+forgotAndSignUpText:{
+color:"white",
+fontSize:11
+},
+loginBtn:{
+width:"80%",
+fontWeight:"bold",
+backgroundColor:"#4AA1B5",
+borderRadius:25,
+height:50,
+alignItems:"center",
+justifyContent:"center",
+marginTop:40,
+marginBottom:10
+},
+loginText: {
+    color:"#FFF"
+},
+errorText: {
+  color: 'red',
+  fontSize: 12,
+  marginBottom: 10,
+},
+});
+export default Login;
