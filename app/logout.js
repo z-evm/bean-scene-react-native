98de18e0ea8a001aee317dc263c232573235538f
Alexander Maniago <alexandermaniago@gmail.com>
2024-11-24 20:07:52 +1100

    Made sure that all tokens are deleted once the user logs out

--- app/logout.js
+++ app/logout.js
@@ -1,6 +1,21 @@
-import { useNavigation } from '@react-navigation/native';
+import {Platform} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from 'expo-secure-store';
 
 export function Logout({navigation}) {
-    navigation.navigate('Login');
+    const deleteUsers = async () => {
+      if (Platform.OS === 'android') {
+        await SecureStore.deleteItemAsync('secure_token');
+      } else {
+        await AsyncStorage.removeItem('secure_token');
+      }
+    }
+    try {
+        deleteUsers();
+        navigation.navigate('Login');
+    }
+    catch(exception) {
+        console.log(exception)
+    }
 }
 export default Logout;
